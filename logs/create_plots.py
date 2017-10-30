a1d03637a5989ca80423bbd4b9d7a3b12da06afd
Michael Harrington <michaeltoddharrington@gmail.com>
2017-10-29 23:13:18 -0500

    plotting

--- logs/create_plots.py
+++ logs/create_plots.py
@@ -5,16 +5,15 @@ from collections import defaultdict
 import argparse
 
 class Generation:
-    def __init__(self, best_fitness, avg_fitness, avg_adaption):
+    def __init__(self, best_fitness, avg_fitness):
         self.best_fitness = best_fitness
         self.avg_fitness  = avg_fitness
-        self.avg_adaption = avg_adaption
 
-def save_box_plot(data, image_name, window_size):
+def save_box_plot(data, image_name, window_size, fit_name):
     fig, ax = plt.subplots(1,1)
     boxplot(data)
     title('Fitness Over the Generations, {}'.format(image_name[6:-4]))
-    ylabel('Fitness')
+    ylabel('Fitness {}'.format(fit_name))
     xlabel('Evaluation Bucket (bucket size = {} evals)'.format(window_size))
 
     for index, label in enumerate(ax.get_xticklabels()):
@@ -33,7 +32,7 @@ def stepify_data(ys):
 
     return y
 
-def save_step_plot(raw_data, image_name, window_size):
+def save_step_plot(raw_data, image_name, window_size, fit_name):
     fix, ax = plt.subplots(1,1)
     best_data = condense_data(raw_data, window_size, lambda g: g.best_fitness)
     
@@ -44,8 +43,8 @@ def save_step_plot(raw_data, image_name, window_size):
     data = condense_data(raw_data, window_size, lambda g: g.avg_fitness)
     boxplot(data)
 
-    title('All Time Best Fitness, Average Fitness Over the Generations')
-    ylabel('Fitness')
+    title('All Time Best Fitness, Average Fitness Over the Generations ({})'.format(fit_name))
+    ylabel('Fitness {}'.format(fit_name))
     xlabel('Evaluation Bucket (bucket size = {} evals)'.format(window_size))
     for index, label in enumerate(ax.get_xticklabels()):
         if index % 2 != 0:
@@ -61,7 +60,7 @@ def condense_data(raw_data, window_size, f):
 
     return data
 
-def parse_data_dict(result_filename):
+def parse_data_dict(result_filename, col_num):
     data = defaultdict(list) # Key: evals, Value: [Best Fitness]
     with open(result_filename) as result_file:
 
@@ -72,9 +71,9 @@ def parse_data_dict(result_filename):
             if line == '':
                 capture_results = False
             if capture_results:
-                evals, avg_fitness, best_fitness = map(float, line.split())
-                avg_adaptive = 1
-                data[evals].append(Generation(best_fitness, avg_fitness, avg_adaptive))
+                evals, *avg_best = map(float, line.split())
+                avg_fitness, best_fitness = avg_best[2*col_num : 2 + 2*(col_num)]
+                data[evals].append(Generation(best_fitness, avg_fitness))
             else:
                 if line.startswith('[Run '):
                     capture_results = True
@@ -84,14 +83,16 @@ def parse_data_dict(result_filename):
 def main():
     parser = argparse.ArgumentParser('Create plots for a results file')
     parser.add_argument('result_file', help='Result file to parse and create result file for')
+    parser.add_argument('num_col', type=int, help='Fitness to select')
+    parser.add_argument('fit_name', help='name of fitness to generate graph on')
     parser.add_argument('-w', '--window_size', default=250, type=int, help='Result file to parse and create result file for')
 
     args = parser.parse_args()
 
-    raw_data = parse_data_dict(args.result_file)
+    raw_data = parse_data_dict(args.result_file, args.num_col)
     windowed_data = condense_data(raw_data, args.window_size, lambda g: g.best_fitness)
-    save_box_plot(windowed_data, 'plots/' + args.result_file.replace('.txt', '_box.png'), args.window_size)
-    save_step_plot(raw_data, 'plots/' + args.result_file.replace('.txt', '_step.png'), args.window_size)
+    save_box_plot(windowed_data, 'plots/' + args.result_file.replace('.txt', '_box.png'), args.window_size, args.fit_name)
+    save_step_plot(raw_data, 'plots/' + args.result_file.replace('.txt', '_step.png'), args.window_size, args.fit_name)
 
 if __name__ == '__main__':
     main()
